c8adb8ad207e6542fe6f397549ecafe588199014
Malecki <vertiavo@gmail.com>
2015-11-16 15:31:48 +0100

    First project.

---
+++ 1/desktop/src/pl/edu/pb/gamedev/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.edu.pb.gamedev.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.edu.pb.gamedev.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
